5f65b93c4ad15a980ef9fc07a4b566ea48c9064c
Bozhidar Valchev <bvalch@gmail.com>
2022-10-15 17:05:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Register from './components/register.js'
+import Login from './components/login.js'
+import RequireAuth from './components/RequireAuth.js';
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout.js';
+import Hikes from './components/Hikes';
+import NavBar from './components/Navbar.js';
+import Profile from './components/Profile.js';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+
+      <Route path="/" element={<Layout />}>
+        <Route path="navbar" element={<NavBar />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        </Route>
+
+
+
+
+        <Route element={<RequireAuth />}>
+
+          <Route path="/hikes" element={<Hikes />} />
+          <Route path="/profile" element={<Profile />} />
+
+        </Route>
+
+
+
+    </Routes>
+
+
   );
 }
+
+
 
 export default App;
