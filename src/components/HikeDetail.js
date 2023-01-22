51a8446f3599666a8225ede7a7e290bd769744ef
bvalch <bvalch@gmail.com>
2023-01-22 15:48:49 +0000

    working on hike details

--- src/components/HikeDetail.js
+++ src/components/HikeDetail.js
@@ -1,7 +1,32 @@
+import "../css/hikedetail.css"
+
 const HikeDetail=({individualHike})=>{
+    if(individualHike===undefined) return <div> Loading</div>
     
     console.log(individualHike)
 
-    return("asd")
+    return(
+    <div className="hikedetail">
+
+        <div className="from-to-container">
+            <div className="left-container">
+            <div className="from-to">{ individualHike.hikeOrigin.split(",")[0]}</div> <div className="from-to">to</div> <div className="from-to">{ individualHike.hikeDestination.split(",")[0]}</div></div>
+
+            <div className="right-container">
+                     <div className ="date-time-join">
+               {individualHike.hikeDate} at {individualHike.hikeTime}
+                      </div>
+                      <div className ="date-time-join">
+                        Transport : {individualHike.hikeTransport} 
+                      </div>
+                      <div className ="date-time-join">
+                        Organiser: {individualHike.hikeOwner} 
+                      </div>
+             </div>
+        </div>
+      
+
+        <div className="hike-description">{individualHike.hikeInfo}</div>
+    </div>)
 };
 export default HikeDetail
