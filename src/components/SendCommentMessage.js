8f9ad17df9c10cb9c48a4aa1305f7d68199df87e
bvalch <bvalch@gmail.com>
2023-01-23 00:09:49 +0000

    client recieves comment and updated hike from server

--- src/components/SendCommentMessage.js
+++ src/components/SendCommentMessage.js
@@ -1,48 +1,47 @@
 import "../css/sendcommesg.css";
 import { useState } from "react";
-import useAxiosRefresh from "../hooks/useAxiosRefresh"
+import useAxiosRefresh from "../hooks/useAxiosRefresh";
 import { useNavigate } from "react-router-dom";
-import { axiosRefresh } from "../api/axios";
 
+const SendCommentMessage = ({ hikeId }) => {
+  const [comment, setComment] = useState("");
+  const axios = useAxiosRefresh();
 
-
-const SendCommentMessage = ({hikeId}) => {
-    const [comment, setComment]=useState("")
-
-    const handleCommentSubmit= async ()=>{
-        const currentTime = new Date().toJSON().slice(0,19).replace('T',' ')
-        const commentObject = {
-            hikeToComment:hikeId,
-            comment,
-            time:currentTime
-        }
-        try{await axiosRefresh.post("/hikes/comment",
-        JSON.stringify(commentObject)
-        
+  const handleCommentSubmit = async () => {
+    const currentTime = new Date().toJSON().slice(0, 19).replace("T", " ");
+    const commentObject = {
+      hikeToComment: hikeId,
+      comment,
+      time: currentTime,
+    };
+    try {
+      const response = await axios.post(
+        "/hikes/comment",
+        JSON.stringify(commentObject),
         {
-          withCredentials:true
-        })
-
-
-        }catch(err){    
-    console.log(err)
-
-}
+          withCredentials: true,
+        }
+      )
+      console.log(response.data)
+    } catch (err) {
+      console.log(err);
     }
-
-
-
+  };
 
   return (
     <div className="add-comment-box">
-
-
-<div class="input-container">
-  <textarea value={comment} onChange={(e)=>setComment(e.target.value)} class="input-field"></textarea>
-</div>
+      <div class="input-container">
+        <textarea
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          class="input-field"
+        ></textarea>
+      </div>
 
       <div className="submit-comment-container">
-        <div onClick={handleCommentSubmit} className="submit-comment">Submit</div>
+        <div onClick={handleCommentSubmit} className="submit-comment">
+          Submit
+        </div>
       </div>
     </div>
   );
